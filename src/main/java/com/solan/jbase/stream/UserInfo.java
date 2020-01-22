ca64003c6c4d22ef5b153f3ac88c6b93fdd76b0b
solan30003 <solan30003@outlook.com>
2020-01-22 17:25:35 +0800

    添加collect应用

--- src/main/java/com/solan/jbase/stream/UserInfo.java
+++ src/main/java/com/solan/jbase/stream/UserInfo.java
@@ -1,9 +1,16 @@
 package com.solan.jbase.stream;
 
+import cn.hutool.core.util.RandomUtil;
+import com.google.common.collect.Lists;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author hyl
@@ -12,7 +19,6 @@ import lombok.ToString;
  */
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @ToString
 public class UserInfo {
     private int id;
@@ -20,4 +26,23 @@ public class UserInfo {
     private String address;
     private float salary;
     private int deptId;
+
+    /**
+     * 创建爱你模拟数据
+     *
+     * @param total 总数
+     * @return
+     */
+    public static List<UserInfo> build(Integer total) {
+        int max = total == null ? 10 : total;
+        List<UserInfo> list = Lists.newArrayList();
+        for (int i = 1; i < max + 1; i++) {
+            list.add(new UserInfo(i,
+                    "solan" + i,
+                    "江苏省南京市",
+                    (float) RandomUtil.randomDouble(1000, 3000),
+                    RandomUtil.randomInt(1, 5)));
+        }
+        return list;
+    }
 }
